05a16b24340fc705a7a88a31011960d34326ead1
seandurban <seandurban10@gmail.com>
2020-02-28 14:42:11 +0000

    Added smtp validation

--- validation/emailValidator.js
+++ validation/emailValidator.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
+const net = require('net');
+const dns = require('dns'); 
+const smtpAddresses = require('./smtpConfig').smtpAddresses;
 
 async function validateEmail(email) {
-
+    let domain = getDomain(email);
     let regexValidationRes = regexValidation(email);
-    let domainValidationRes = await domainValidation(email.split('@')[1]);
-    let smtpValidationRes = smtpValidation('smtp.' + email.split('@')[1]);
-
+    let domainValidationRes = await domainValidation(domain);
+    let smtpValidationRes = await smtpValidation(domain);
     let valid = regexValidationRes.valid && domainValidationRes.valid && smtpValidationRes.valid;
 
     let resultFormat = { 
@@ -31,17 +33,52 @@ function regexValidation(email) {
 
 async function domainValidation(domain) {
     try {
-        await axios.get('http://'+ domain, {"accept" : "*/*"});
+       await axios.get('http://'+ domain);
+       return {valid: true};
     }
     catch (err) {
         return {valid : false, reason : err.code};
     }
-    return {valid : true};
 }
 
-function smtpValidation(hostname) {
-    return {valid : true};
+function smtpValidation(domain) {
+    // Net library doesn't have promise support out of the box so have to implement it
+    return new Promise((resolve) => {
+        // attempt lookup on common smtp addresses
+        let domainName = getDomainWithoutExtension(domain);
+        let smtpAddress = smtpAddresses[domainName];
+        if(!smtpAddress) smtpAddress = 'smtp.' + domain;
+            let connection= net.createConnection(587, smtpAddress, () => {
+                    connection.end();
+                    resolve({valid:true});
+            });
+
+            // This will catch all errors, timeout error can take a number of seconds
+            connection.on('error', (err) => {
+                resolve({valid : false, reason : err.code});
+            });
+    });
 }
 
+// Simple splitting of string to get domain => assumes email is in standard form XYZ@domain.com
+function getDomain(email) {
+    let domain = email.split('@')[1];
+    if(!domain) domain = '';
+    return domain;
+}
+
+// Simple splitting of string to get domain name  => assumes domain is in standard form domain.com
+function getDomainWithoutExtension(domain) {
+    let domainName = domain.split('.')[0];
+    if(!domainName) domainName = '';
+    return domainName;
+}
+
+// Uses DNS to retrieve MX addresses associated with a domain
+function getMxRecord(domain) {
+    dns.resolveMx(domain, (err, addresses ) => {
+        return addresses;
+    });
+}
 
 module.exports = {validateEmail};
